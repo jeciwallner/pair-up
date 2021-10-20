7dd0c2c1979e8b53bd5b3cc7454a5d6d497df77d
JeciHume <jecihume@gmail.com>
2021-10-20 17:59:40 +0200

    Set up project

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+// this is my main component where the app is going to be rendered.
+
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
