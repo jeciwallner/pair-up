063563775761823d4c906c57f55996c567b558b3
JeciHume <jecihume@gmail.com>
2021-11-20 22:04:33 +0100

    🔥 Remove unused CSS

--- styles/globalStyles.js
+++ styles/globalStyles.js
@@ -1,67 +1,76 @@
 import { css } from '@emotion/react';
 
-const reset = css`
-  html {
-    box-sizing: border-box;
-    font-size: 16px;
-  }
+// background: #fdfdf6;
+// font-family: fantasy;
 
-  *,
-  *::before,
-  *::after {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-  html {
-    height: 100%;
-  }
-  body {
-    min-height: 100%;
-    display: grid;
-    grid-template-rows: 1fr auto;
-    background: blanchedalmond;
-    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande',
-      'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+const reset = css`
+  #__next {
     width: 100%;
-    height: 100%;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
   }
 `;
 
 export const myGlobalStyles = css`
   ${reset}
-  /* Header: */
-  .navStyles {
-    display: flex;
-    gap: 10px;
-    background-color: turquoise;
-    color: black;
-  }
-  .wrapper {
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-  }
-  .container {
-    display: grid, inline-grid;
+
+  button {
+    background-color: #499be7;
+    padding: 0.5em;
+    border-radius: 32px;
+    border-color: #073162;
+    color: #073162;
+    font-size: 1em;
+    width: 25em;
+    max-width: 80vw;
+    cursor: pointer;
+    align-self: center;
+    &:active {
+      transform: scale(1.05);
+    }
+    &:hover {
+      opacity: 0.6;
+    }
   }
 
-  .footerStyles {
-    height: 50px;
-    margin-top: -50px;
-
+  footer {
+    display: flex;
+    padding: 0.5rem;
+    margin-top: 10px;
     gap: 10px;
-    background-color: turquoise;
-    color: black;
+    position: sticky;
   }
-  .errorStyles {
+
+  error {
     color: #ff0077;
   }
-  .grid-container {
-    display: inline-grid;
+
+  .column {
+    float: left;
+    width: 33%;
+    padding: 5px;
+  }
+
+  /* Clear floats after image containers */
+  .row::after {
+    content: '';
+    clear: both;
+    display: table;
+  }
+  .dropdown {
+    position: relative;
+    display: inline-block;
+  }
+
+  .dropdown-content {
+    display: none;
+    position: absolute;
+    background-color: #f9f9f9;
+    min-width: 160px;
+    box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+    padding: 12px 16px;
+    z-index: 1;
+  }
+
+  .dropdown:hover .dropdown-content {
+    display: block;
   }
 `;
