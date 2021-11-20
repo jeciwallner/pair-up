08d7382ad6c8208f71e1d940cd2253391b8b71fa
JeciHume <jecihume@gmail.com>
2021-11-20 22:00:26 +0100

    Set up Bootstrap

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,14 @@
 // this is my main component where the app is going to be rendered.
 
-import { css, Global } from '@emotion/react';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Global } from '@emotion/react';
 import Head from 'next/head';
+import { myGlobalStyles } from '../styles/globalStyles';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Global
-        styles={css`
-          *,
-          *::before,
-          *::after {
-            box-sizing: border-box;
-          }
-          body {
-            margin: 0;
-          }
-        `}
-      />
+      <Global styles={myGlobalStyles} />
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
