400983d9e1f523af88d6a9e7dc378eeff2d7ce54
JeciHume <jecihume@gmail.com>
2021-11-22 10:18:54 +0100

    🚧 Temporarily remove code to be able to deploy

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -1,27 +1,27 @@
 import { createUser, getUsers } from '../../../util/database';
 
-export default function user(props) {}
+export default function user() {}
 
-export async function getServerSideProps(context) {
-  // return { props: {} };
-  const { getStyles } = await import('../../util/database');
+export async function getServerSideProps() {
+  return { props: {} };
+  //   const { getStyles } = await import('../../util/database');
 
-  const users = await getStyles();
+  //   const users = await getStyles();
 
-  export async function handler(req, res) {
-    if (req.method === 'GET') {
-      const users = await getUsers();
-      return res.status(200).json(styles);
-    } else if (req.method === 'POST') {
-      const body = req.body;
-      const createdUser = await createUser({
-        name: body.userName,
-        styles: body.userStyles,
-      });
+  //   export async function handler(req, res) {
+  //     if (req.method === 'GET') {
+  //       const users = await getUsers();
+  //       return res.status(200).json();
+  //     } else if (req.method === 'POST') {
+  //       const body = req.body;
+  //       const createdUser = await createUser({
+  //         name: body.userName,
+  //         styles: body.userStyles,
+  //       });
 
-      return res.status(200).json(createdUser);
-    }
+  //       return res.status(200).json(createdUser);
+  //     }
 
-    return res.status(405);
-  }
+  //     return res.status(405);
+  //   }
 }
